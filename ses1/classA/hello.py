f975f00122118b54adf79ccb8684c9770de2bc9c
clbokea <clbo@kea.dk>
2021-02-02 15:55:36 +0100

    ses1 over

--- ses1/classA/hello.py
+++ ses1/classA/hello.py
@@ -1,4 +1,13 @@
-def main():
-    return 'Hello'
 
-main()
+
+import sys
+
+def xxx(name):
+
+    if name == 'Alice' or name == 'Jens':
+        print("Hollo " + name)
+    else:
+        # print('You are not a real person')
+        slkdfjsdlkfjsdj
+    
+xxx(sys.argv[1])
--- ses1/classB/hello.py
+++ ses1/classB/hello.py
@@ -1,4 +1,10 @@
-def main():
-    return 'Hello'
+import sys
 
-main()
+def main(name):
+
+    if name == 'Alice' or name == 'Jens':
+        print('Hello ' + name)
+    else:
+        print('You are not a real human!')
+
+main(sys.argv[1])
