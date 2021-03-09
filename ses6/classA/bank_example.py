532b4ff5150e0a9bb34e79efe010f87da5b31714
clbokea <clbo@kea.dk>
2021-03-09 11:21:15 +0100

    fin

---
+++ ses6/classA/bank_example.py
@@ -0,0 +1,41 @@
+""" Bank Exercise 
+
+Create a 
+
+* Bank class
+* Account Class
+* Customer class
+
+The bank class should be able to hold many account.
+You should be able to add new accounts.
+The Account class should have relevant details.
+The Customer class Should also have relevant details.
+
+Stick to the techniques we have covered so far.
+
+
+"""
+
+class Bank:
+    def __init__(self):
+        self.accounts = []
+
+class Account:
+    def __init__(self, no, cust):
+        self.no = no
+        self.cust = cust
+
+class Customer:
+    def __init__(self, *args):
+        if len(args) == 1:
+            self.name = args[0]
+        else:
+            self.name = args[0]
+            self.age = args[1]
+    
+    def __repr__(self):
+        return self.name
+
+    def __str__(self):
+        pass
+
